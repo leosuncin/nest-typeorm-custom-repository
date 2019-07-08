e49b31dd48a449f8eb02e1355926c641ea816b6d
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2019-07-08 10:51:15 -0600

    feat(task): Add module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TaskModule } from './task/task.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot()],
+  imports: [TypeOrmModule.forRoot(), TaskModule],
   controllers: [AppController],
   providers: [AppService],
 })

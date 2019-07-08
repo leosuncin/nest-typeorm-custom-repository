591440edccbf7a686a9c36f3eb211396527a477a
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2019-07-08 10:58:53 -0600

    feat(task): Add custom repository

--- src/task/task.module.ts
+++ src/task/task.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { Task } from './task.entity';
+import { TaskRepository } from './task.repository';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Task])],
+  imports: [TypeOrmModule.forFeature([Task, TaskRepository])],
 })
 export class TaskModule {}
---
+++ src/task/task.repository.ts
@@ -0,0 +1,14 @@
+import { EntityRepository, Repository } from 'typeorm';
+
+import { Task } from './task.entity';
+
+@EntityRepository(Task)
+export class TaskRepository extends Repository<Task> {
+  findDone() {
+    return this.find({done: true});
+  }
+
+  findPending() {
+    return this.find({done: false});
+  }
+}

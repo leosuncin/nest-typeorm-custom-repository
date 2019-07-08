09a43f228d362a8b52246be24b63b31e37a819fb
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2019-07-08 11:22:46 -0600

    feat(task): Add service

--- src/task/task.module.ts
+++ src/task/task.module.ts
@@ -3,8 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { Task } from './task.entity';
 import { TaskRepository } from './task.repository';
+import { TaskService } from './task.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Task, TaskRepository])],
+  providers: [TaskService],
 })
 export class TaskModule {}
---
+++ src/task/task.service.ts
@@ -0,0 +1,58 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+
+import { TaskRepository } from './task.repository';
+import { TaskCreate } from './dto/task-create.dto';
+import { TaskUpdate } from './dto/task-update.dto';
+
+@Injectable()
+export class TaskService {
+  constructor(
+    @InjectRepository(TaskRepository)
+    private readonly taskRepository: TaskRepository,
+  ) {}
+
+  findAll() {
+    return this.taskRepository.find();
+  }
+
+  findAllDone() {
+    return this.taskRepository.findDone();
+  }
+
+  findAllPending() {
+    return this.taskRepository.findPending();
+  }
+
+  async get(id: number) {
+    const task = await this.taskRepository.findOne(id);
+
+    if (!task) {
+      throw new NotFoundException(`There isn't any task with id: ${id}`);
+    }
+
+    return task;
+  }
+
+  create(newTask: TaskCreate) {
+    const task = this.taskRepository.create(newTask);
+
+    return this.taskRepository.save(task);
+  }
+
+  async update(id: number, updates: TaskUpdate) {
+    const task = await this.taskRepository.findOne(id);
+
+    if (!task) {
+      throw new NotFoundException(`There isn't any task with id: ${id}`);
+    }
+
+    Object.assign(task, updates);
+
+    return this.taskRepository.save(task);
+  }
+
+  remove(id: number) {
+    return this.taskRepository.delete(id);
+  }
+}

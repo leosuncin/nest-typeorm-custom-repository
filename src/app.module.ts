962864ad574077613e42d7b2f3f63b4c353da03a
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2022-06-23 15:46:28 -0600

    feat: use data source options

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { options } from './data-source';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TaskModule } from './task/task.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), TaskModule],
+  imports: [TypeOrmModule.forRoot(options), TaskModule],
   controllers: [AppController],
   providers: [AppService],
 })
